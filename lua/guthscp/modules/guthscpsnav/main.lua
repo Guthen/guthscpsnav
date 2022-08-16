b8101870dfccf799dc976f3ef1e50322ce8ba9ba
Guthen <33220603+Guthen@users.noreply.github.com>
2022-08-16 21:56:17 +0200

    [moved] `MODULE.config` to `MODULE.menu.config`

--- lua/guthscp/modules/guthscpsnav/main.lua
+++ lua/guthscp/modules/guthscpsnav/main.lua
@@ -14,84 +14,116 @@ local MODULE = {
 	},
 }
 
-MODULE.config = {
-	form = {
-		{
-			type = "Category",
-			name = "General",
+MODULE.menu = {
+	--  config
+	config = {
+		form = {
+			{
+				type = "Category",
+				name = "General",
+			},
+			{
+				type = "TextEntry",
+				name = "Key",
+				id = "key",
+				default = "M",
+				desc = "Key to press to hide and show the S-NAV when it's equipped",
+			},
+			{
+				type = "NumWang",
+				name = "Blink Time",
+				id = "blink_time",
+				default = .33,
+				desc = "Time between each S-NAV screen refresh and especially to refresh the hostiles and their positions",
+				decimals = 2,
+				min = 0,
+			},
+			{
+				type = "NumWang",
+				name = "Show Distance",
+				id = "hostiles_dist",
+				default = 724,
+				desc = "Maximum distance where hostiles (SCPs & NPCs) will be shown on the S-NAV"
+			},
+			{
+				type = "CheckBox",
+				name = "Show Positions",
+				id = "show_hostiles_pos",
+				desc = "If checked, hostiles positions (SCPs & NPCs) will be revealed on the S-NAV"
+			},
+			{
+				type = "CheckBox",
+				name = "Constant Refresh",
+				id = "hostile_constant_refresh",
+				desc = "If checked, hostiles positions and distance will be constantly refreshed, otherwise it will refresh at a constant rate (every time the texts \"blinks\")"
+			},
+			{
+				type = "Category",
+				name = "SCPs",
+			},
+			{
+				type = "CheckBox",
+				name = "Enabled",
+				id = "scps_enabled",
+				default = true,
+				desc = "If checked, SCPs will be shown on the S-NAV Ultimate",
+			},
+			{
+				type = "Category",
+				name = "NPCs",
+			},
+			{
+				type = "CheckBox",
+				name = "Enabled",
+				id = "npcs_enabled",
+				default = false,
+				desc = "If checked, NPCs will be shown on the S-NAV",
+			},
+			{
+				type = "CheckBox",
+				name = "Hostiles Only",
+				id = "npcs_hostile_only",
+				default = true,
+				desc = "If checked, only hostile NPCs will be shown. Since it use `IsEnemyEntityName` internally, this may omit custom NPCs",
+			},
+			guthscp.config.create_apply_button(),
 		},
-		{
-			type = "TextEntry",
-			name = "Key",
-			id = "key",
-			default = "M",
-			desc = "Key to press to hide and show the S-NAV when it's equipped",
-		},
-		{
-			type = "NumWang",
-			name = "Blink Time",
-			id = "blink_time",
-			default = .33,
-			desc = "Time between each S-NAV screen refresh and especially to refresh the hostiles and their positions",
-			decimals = 2,
-			min = 0,
-		},
-		{
-			type = "NumWang",
-			name = "Show Distance",
-			id = "hostiles_dist",
-			default = 724,
-			desc = "Maximum distance where hostiles (SCPs & NPCs) will be shown on the S-NAV"
-		},
-		{
-			type = "CheckBox",
-			name = "Show Positions",
-			id = "show_hostiles_pos",
-			desc = "If checked, hostiles positions (SCPs & NPCs) will be revealed on the S-NAV"
-		},
-		{
-			type = "CheckBox",
-			name = "Constant Refresh",
-			id = "hostile_constant_refresh",
-			desc = "If checked, hostiles positions and distance will be constantly refreshed, otherwise it will refresh at a constant rate (every time the texts \"blinks\")"
-		},
-		{
-			type = "Category",
-			name = "SCPs",
-		},
-		{
-			type = "CheckBox",
-			name = "Enabled",
-			id = "scps_enabled",
-			default = true,
-			desc = "If checked, SCPs will be shown on the S-NAV Ultimate",
-		},
-		{
-			type = "Category",
-			name = "NPCs",
-		},
-		{
-			type = "CheckBox",
-			name = "Enabled",
-			id = "npcs_enabled",
-			default = false,
-			desc = "If checked, NPCs will be shown on the S-NAV",
-		},
-		{
-			type = "CheckBox",
-			name = "Hostiles Only",
-			id = "npcs_hostile_only",
-			default = true,
-			desc = "If checked, only hostile NPCs will be shown. Since it use `IsEnemyEntityName` internally, this may omit custom NPCs",
-		},
-		guthscp.config.create_apply_button(),
+		receive = function( form )
+			guthscp.config.apply( MODULE.id, form, {
+				network = true,
+				save = true,
+			} )
+		end,
 	},
-	receive = function( form )
-		guthscp.config.apply( MODULE.id, form, {
-			network = true,
-			save = true,
-		} )
-	end,
+	--  details
+	details = {
+		{
+			text = "CC-BY-SA",
+			icon = "icon16/page_white_key.png",
+		},
+		"Wiki",
+		{
+			text = "How to create minimaps?",
+			icon = "icon16/map_add.png",
+			url = "https://github.com/Guthen/guthscpsnav/blob/update-to-guthscpbase-remaster/CREATE_MINIMAP.md",
+		},
+		"Social",
+		{
+			text = "Github",
+			icon = "guthscp/icons/github.png",
+			url = "https://github.com/Guthen/guthscpsnav",
+		},
+		{
+			text = "Steam",
+			icon = "guthscp/icons/steam.png",
+			url = "https://steamcommunity.com/sharedfiles/filedetails/?id=2139521265"
+		},
+		{
+			text = "Discord",
+			icon = "guthscp/icons/discord.png",
+			url = "https://discord.gg/Yh5TWvPwhx",
+		},
+	},
 }
 
 guthscp.module.hot_reload( "guthscpsnav" )
